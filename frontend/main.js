d4b03728b1f47faaf239a46001763ffed7f415d0
Pankaj biradar <pankaj28988@gmail.com>
2023-03-10 16:57:18 +0530

    Deployed storage and function to azure

--- frontend/main.js
+++ frontend/main.js
@@ -2,11 +2,12 @@ window.addEventListener('DOMContentLoaded', (event) => {
     getVisitCount();
 })
 
+const functionApiUrl = "https://getresumecounteraz.azurewebsites.net/api/GetResumeCounter?code=IUle30KXZxkkUxikzfdIeW7uj4w_sLKW5JHS7RNm2tt4AzFuaWMJ-Q=="
 const functionApi = 'http://localhost:7071/api/GetResumeCounter';
 
 const getVisitCount = () => {
     let count = 10;
-    fetch(functionApi).then(response => {
+    fetch(functionApiUrl).then(response => {
         return response.json()
     }).then(response => {
         console.log("Website called function API.");
